7ba891fdc3f2edf1665535ecb031a7f0c5b9fbcb
ABBDVD <abbdvd@hotmail.com>
2018-09-26 17:17:59 +0200

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -1,4 +1,15 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+   /* solc: {
+        optimizer: {
+            enabled:    true,
+            runs:       200000
+        }
+    },*/
+  networks: {
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*', // eslint-disable-line camelcase
+    },
+  },
 };
